27524d6db6ee0acaf6a53e55ff2d79e40d7a51e0
Alin Grigorescu <61597725+alingrig87@users.noreply.github.com>
2024-10-23 17:52:11 +0000

    refactor: use async await and modules

--- index.js
+++ index.js
@@ -1,30 +1,12 @@
-const URL = "https://670fe587a85f4164ef2c6100.mockapi.io/products";
+import { getAllProducts } from "./api/products.js";
+import { mapProductToCard } from "./utils/layout.js";
+
 const productsContainer = document.querySelector(".products");
 
 document.addEventListener("DOMContentLoaded", displayProducts);
 
-function displayProducts() {
-  fetch(URL)
-    .then((response) => response.json())
-    .then(
-      (products) =>
-        (productsContainer.innerHTML = products
-          .map(
-            (product) => `
-        <div class="card flex-col  items-center justify-around gap-20">
-            <div class="info  flex-col items-center gap-20">
-                <div class="img">
-                    <img src=${product.imageURL} alt="">
-                </div>
-                <div class="name">
-                    <h4>${product.name}</h4>
-                </div>
-                <div class="price">${product.price} lei</div>
-            </div>
-            <button class="add-to-cart">Adauga in cos</button>
-        </div>
-    `
-          )
-          .join(""))
-    );
+async function displayProducts() {
+  const products = await getAllProducts();
+
+  productsContainer.innerHTML = products.map(mapProductToCard).join("");
 }
--- js/admins.js
+++ js/admins.js
@@ -1,35 +1,24 @@
+import {
+  getAllProducts,
+  getProductById,
+  deleteProduct,
+  updateProduct,
+  addNewProduct,
+} from "../api/products.js";
+import { mapProductToAdminTableRow } from "../utils/layout.js";
+
 const productsTableBody = document
   .getElementById("products-table")
   .querySelector("tbody");
 
 document.addEventListener("DOMContentLoaded", displayProducts);
 
-const URL = "https://670fe587a85f4164ef2c6100.mockapi.io/products";
+async function displayProducts() {
+  const products = await getAllProducts();
 
-function displayProducts() {
-  fetch(URL)
-    .then((response) => response.json())
-    .then(
-      (products) =>
-        (productsTableBody.innerHTML = products
-          .map(
-            (product) => `
-        <tr>
-            <td>${product.name}</td>
-            <td>${product.price}</td>
-            <td><img width="50px" src=${product.imageURL} /></td>
-            <td>${product.details} </td>
-            <td>
-                <button class="edit-btn" data-productId=${product.id}>Edit</button>
-            </td>
-            <td>
-                <button class="delete-btn" data-productId=${product.id}>Delete</button>
-            </td>
-        </tr>
-    `
-          )
-          .join(""))
-    );
+  productsTableBody.innerHTML = products
+    .map(mapProductToAdminTableRow)
+    .join("");
 }
 
 // save new product
@@ -44,7 +33,7 @@ let editMode = false;
 
 saveProductBtn.addEventListener("click", saveProduct);
 
-function saveProduct(event) {
+async function saveProduct(event) {
   event.preventDefault();
 
   const product = {
@@ -55,46 +44,39 @@ function saveProduct(event) {
   };
 
   if (editMode) {
-    fetch(`${URL}/${currentEditableProductId}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(product),
-    }).then(() => {
+    const editedProduct = await updateProduct(
+      product,
+      currentEditableProductId
+    );
+    if (editedProduct !== null) {
       form.reset();
-      displayProducts();
+      await displayProducts();
       editMode = false;
-    });
+    }
   } else {
-    fetch(URL, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(product),
-    }).then(() => displayProducts());
+    const newProduct = await addNewProduct(product);
+    if (newProduct !== null) {
+      form.reset();
+      await displayProducts();
+    }
   }
 }
 
 productsTableBody.addEventListener("click", handleActions);
 
-function handleActions(event) {
+async function handleActions(event) {
   if (event.target.classList.contains("edit-btn")) {
     currentEditableProductId = event.target.getAttribute("data-productId");
-    fetch(`${URL}/${currentEditableProductId}`)
-      .then((response) => response.json())
-      .then((product) => {
-        nameInput.value = product.name;
-        priceInput.value = product.price;
-        imageUrlInput.value = product.imageURL;
-        detailsInput.value = product.details;
-      });
+    const currentProduct = await getProductById(currentEditableProductId);
+
+    nameInput.value = currentProduct.name;
+    priceInput.value = currentProduct.price;
+    imageUrlInput.value = currentProduct.imageURL;
+    detailsInput.value = currentProduct.details;
     editMode = true;
   } else if (event.target.classList.contains("delete-btn")) {
     const id = event.target.getAttribute("data-productId");
-    fetch(`${URL}/${id}`, {
-      method: "DELETE",
-    }).then(() => displayProducts());
+    await deleteProduct(id);
+    await displayProducts();
   }
 }
